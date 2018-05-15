026d1f6de9aef2b5fd0bce831771aa2cf5d74831
TRNG <TRNG@10.80.12.117>
2018-05-15 13:28:09 +0530

    dgh

--- Maven_proj/src/main/java/p1/ABC.java
+++ Maven_proj/src/main/java/p1/ABC.java
@@ -4,7 +4,7 @@ public class ABC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("ABCkdghlfbadfuogybalscd");
+System.out.println("fghdfghsfghsf bherher erwt r trwty wrth rw ");
 	}
 
 }
